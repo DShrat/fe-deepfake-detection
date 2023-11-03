90d9968c83d5d6ee761389d028d9aa1f5fd5f5f9
D Shrat <dshrat29@gmail.com>
2023-11-03 15:53:09 +0700

    push baru

--- src/components/cardLabel.js
+++ src/components/cardLabel.js
@@ -2,23 +2,6 @@ import { Row, Col } from "react-bootstrap";
 import "./style.css";
 
 function CardLabel(){
-    // const storedImage = localStorage.getItem('image');
-    // const storedLabel = JSON.parse(localStorage.getItem('label'));
-
-    // return(
-        
-    //     <Row className='square border border-dark'>
-    //         <Col className="d-flex align-items-center justify-content-center">
-    //             <div className="imageShow">
-    //                 <img src={storedImage} alt="Logo" style={{height: 100, width: 100}}/>
-    //             </div>
-    //         </Col>
-    //         <Col className="d-flex align-items-center justify-content-center" >
-    //             <div>{storedLabel}</div>
-    //         </Col>
-    //     </Row>
-    // )
-
     const imageLabelPairs = JSON.parse(localStorage.getItem('imageLabelPairs')) || [];
 
     window.onbeforeunload = function() {
